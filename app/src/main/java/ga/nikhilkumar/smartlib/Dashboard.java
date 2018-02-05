7ccd3428488a94ebb2a2aa83d953e949503d8b7f
Nikhil <nikhil.nikhil.muz@gmail.com>
2018-02-06 04:26:50 +0530

    added login activity

--- app/src/main/java/ga/nikhilkumar/smartlib/Dashboard.java
+++ app/src/main/java/ga/nikhilkumar/smartlib/Dashboard.java
@@ -7,17 +7,9 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class Dashboard extends AppCompatActivity {
-Button toast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        toast=(Button) findViewById(R.id.toast);
-        toast.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(Dashboard.this, "hi", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
